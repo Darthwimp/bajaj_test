771b32661d4c5e55b51edd7e1cf41d3d1f8666f7
darthwimp <debargha2003.das@gmail.com>
2024-09-22 16:15:46 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express();
+
+app.listen(5000, ()=>{
+    console.log("server started at port 5000");
+})
+
+app.get("/", (req,res)=>{
+    res.send("Server is ready");
+})
